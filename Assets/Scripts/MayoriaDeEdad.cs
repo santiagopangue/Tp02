705abc997c6b8a420e0a51983cecf65ea07e7e10
santiagopangue <santiagopangue@gmail.com>
2022-04-02 18:06:23 -0300

    Tp02Completed

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadUsuario;
+    public Text miTexto;
+
+    void Start()
+    {
+        if (edadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            miTexto.text = "Es menor de edad";
+            Debug.Log("Es menor de edad");
+        }
+    }
+
+}
---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    string contraseñaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContraseña()
+    {
+        contraseñaUsuario = ingresoUsuario.text;
+        if (contraseñaUsuario == contraseñaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+
+
+}
